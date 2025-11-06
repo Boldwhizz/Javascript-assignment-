a7bc2c6986eef7dfb75fce2d0ad5caaf21798843
Boldwhizz <Boldwhizz@gmail.com>
2025-11-06 21:36:15 +0100

    updated

---
+++ script.js
@@ -0,0 +1,56 @@
+let a = 4
+let b = 6
+let sum = a + b
+console.log(sum);
+
+let sub = b-a
+console.log(sub);
+
+let mut = a * b
+console.log(mut)
+
+let div = b / a
+console.log(div)
+
+let mod = b % a
+console.log(mod)
+
+let exp = b ** a
+console.log(exp)
+
+let x = 8
+x += a
+console.log(a)
+
+x -= a
+console.log(a)
+
+x *= a
+console.log(a)
+
+x /= a
+console.log(a)
+
+let c = 2
+let d = 5
+console.log(c == d)
+
+console.log(c === d)
+
+console.log(c != d)
+
+console.log(c !== d)
+
+console.log(c > d)
+
+console.log(c < d)
+
+console.log(c <= d)
+
+console.log(c >= d)
+
+console.log(c == d && d > c)
+
+console.log(c == d || d > c)
+
+console.log(!c == d)
